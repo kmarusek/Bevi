7ee8c28bf499650bb2aaef358a62e88d8fc1c644
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2020-10-07 13:29:10 +0100

    BEVIDAB-1 - added vue

---
+++ web/app/themes/bevi/webpack.mix.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const mix = require('laravel-mix');
+const path = require('path');
+require('mix-tailwindcss');
+require('laravel-mix-eslint');
+
+mix.browserSync({
+  proxy: 'bevi.test',
+  open: true,
+})
+  .setResourceRoot('/app/themes/reis/dist')
+  .setPublicPath('./dist')
+  .webpackConfig({
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'resources'),
+      },
+    },
+  })
+  .js('resources/assets/scripts/main.js', 'dist/scripts')
+  .eslint()
+  .options({
+    processCssUrls: false,
+  })
+  .tailwind('resources/assets/styles/tailwind.config.js')
+  .sass('resources/assets/styles/main.scss', 'dist/styles')
+  .copyDirectory('resources/assets/fonts', 'dist/fonts')
+  .copyDirectory('resources/assets/images', 'dist/images');
+
+if (mix.inProduction()) {
+  mix.version();
+}
