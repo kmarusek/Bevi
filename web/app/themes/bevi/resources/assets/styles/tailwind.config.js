8c264cc9aa7923ed5f01ee62729f8111a651d59b
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2020-10-09 12:04:43 +0100

    BEVIDAB-1 - added colours

--- web/app/themes/bevi/resources/assets/styles/tailwind.config.js
+++ web/app/themes/bevi/resources/assets/styles/tailwind.config.js
@@ -7,7 +7,53 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#00a4d8',
+        primary: '#246EFF',
+        green: {
+          100: '#DBFFA8',
+          200: '#C7E4A0',
+          default: '#73BC25',
+          500: '#1A8C25',
+          600: '#105128',
+        },
+        blue: {
+          100: '#D4E4F8',
+          200: '#A8C3E4',
+          default: '#246EFF',
+          500: '#174CBB',
+          600: '#00223E',
+        },
+        purple: {
+          200: '#E9D9E8',
+          default: '#4B273A',
+        },
+        red: {
+          200: '#F7DBDA',
+          default: '#DC3A32',
+          500: '#AB2328',
+        },
+        orange: {
+          100: '#FCEDDA',
+          200: '#FFD7B1',
+          default: '#FFA24E',
+          500: '#FF7800',
+          600: '#00223E',
+        },
+        yellow: {
+          100: '#FCF5CB',
+          200: '#FCF1A7',
+          default: '#FFD864',
+          500: '#FFC002',
+          600: '#B28600',
+        },
+        grey: {
+          100: '#FAFAFA',
+          200: '#F2F2F2',
+          300: '#E5E5E5',
+          400: '#CCCCCC',
+          default: '#999999',
+          700: '#666666',
+          800: '#B28600',
+        }
       }
     },
   },
