19199d20bbb70a8e4d8171466ad39b709240c4f7
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2020-10-20 18:31:43 +0100

    BEVIDAB-13 - updated animation

---
+++ web/app/themes/bevi/resources/mixins/GSAPFade.js
@@ -0,0 +1,41 @@
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+export default {
+  mounted() {
+    this.startTextFade();
+  },
+  methods: {
+    startTextFade() {
+      gsap.utils.toArray('.gsap-fade-section').forEach((section) => {
+        const elems = section.querySelectorAll('.gsap-fade');
+        
+        // Set things up
+        gsap.set(elems, { y: 50, opacity: 0 });
+        
+        ScrollTrigger.create({
+          trigger: section,
+          start: 'top 60%',
+          onEnter: () => gsap.to(elems, {
+            y: 0,
+            opacity: 1,
+            duration: 1,
+            stagger: 0.2,
+            delay: 0.3,
+            ease: 'power3.out',
+            overwrite: 'auto',
+          }),
+          onLeaveBack: () => gsap.to(elems, {
+            y: 50,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2,
+            delay: 0.3,
+            ease: 'power3.out',
+            overwrite: 'auto',
+          }),
+        });
+      });
+    },
+  },
+};
