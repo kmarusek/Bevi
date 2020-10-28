be579448d6a2fa5fec7483e4535f0295fd6c2cf9
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2020-10-28 17:00:32 +0000

    BEVIDAB-24 - component created

---
+++ web/app/themes/bevi/resources/views/modules/careers_component.blade.php
@@ -0,0 +1,14 @@
+<script src="https://www.workable.com/assets/embed.js" type="text/javascript"></script>
+<script type="text/javascript" charset="utf-8">
+whr(document).ready(function(){
+// whr_embed(250461, {detail: 'titles', base: 'jobs', zoom: 'country', grouping: 'none'});
+whr_embed(250461, {detail: 'titles', base: 'departments', zoom: 'city', });
+});
+</script>
+
+<section class="bg-gray-200 gsap-fade-section">
+  <div class="py-20 lg:py-32 container">
+    <h3 class="text-4xl mb-10 text-blue-600 font-medium gsap-fade">Open Positions</h3>
+    <div id="whr_embed_hook" class="opprtunities-embed gsap-fade"></div>
+</div>
+</section>
