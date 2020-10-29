c8f03ab42049f38eb8e8dff0e2a164953dd1c32e
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2020-10-29 16:34:44 +0000

    BEVIDAB-23 - added bullets

--- web/app/themes/bevi/resources/components/modules/FormComponent/FormComponent.vue
+++ web/app/themes/bevi/resources/components/modules/FormComponent/FormComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <section :class="{ 'bg-blue-100' : block.show_blue_background }">
-    <div class="py-20 lg:py-32 container xl:max-w-5xl flex flex-col md:flex-row">
+    <div class="py-20 lg:py-32 container xl:max-w-6xl flex flex-col md:flex-row">
       <div class="md:w-5/12 lg:pr-16">
         <h3 class="text-3xl md:text-4xl mb-4 text-blue-600 font-semibold leading-tight">
           {{ block.title }}
@@ -10,15 +10,15 @@
           <div
             v-for="bullet in block.bullets"
             :key="bullet.id"
-            class="flex w-full sm:my-8"
+            class="flex w-full my-4 sm:my-10"
           >
-            <div class="w-1/4 sm:w-1/5">
+            <div class="w-1/5 pr-2 md:pr-0">
               <img
                 :src="bullet.icon.sizes.thumbnail"
-                class="mb-2 mt-8 sm:my-0"
+                alt=""
               >
             </div>
-            <div class="sm:w-4/5 sm:px-6">
+            <div class="w-4/5 sm:px-6">
               <h5 class="font-semibold text-lg tracking-wide text-blue-600">
                 {{ bullet.title }}
               </h5>
