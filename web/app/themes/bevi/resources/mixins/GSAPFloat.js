b5a3d1c22b7f1154e9851c3515f0ec7ba895fed4
Tim Nokes <tim.nokes@e3creative.co.uk>
2020-11-04 10:46:37 +0000

    BEVIDAB-20 updated gsap fade mixin

--- web/app/themes/bevi/resources/mixins/GSAPFloat.js
+++ web/app/themes/bevi/resources/mixins/GSAPFloat.js
@@ -13,23 +13,40 @@ export default {
       
       const randomX = random(0, 10);
       const randomY = random(0, 10);
-      const randomTime = random(3, 4);
-      const randomAngle = random(-5, 5);
-
+      const randomTime = random(3, 5);
+      const randomTime2 = random(5, 10);
+      const randomAngle = random(-3, 3);
       const float = document.querySelectorAll('.gsap-float');
-      const floatTl = gsap.timeline({ repeat: -1, yoyo: true });
-      floatTl.set(float, {
-        x: 0,
-        y: 0,
-        rotation: 0,
+
+      function rotate(target, direction) {
+        gsap.to(target, randomTime2(), {
+          rotation: randomAngle(direction),
+          ease: Sine.easeInOut,
+          onComplete: rotate,
+          onCompleteParams: [target, direction * -1],
+        });
+      }
+      function moveX(target, direction) {
+        gsap.to(target, randomTime(), {
+          x: randomX(direction),
+          ease: Sine.easeInOut,
+          onComplete: moveX,
+          onCompleteParams: [target, direction * -1],
+        });
+      }
+      function moveY(target, direction) {
+        gsap.to(target, randomTime(), {
+          y: randomY(direction),
+          ease: Sine.easeInOut,
+          onComplete: moveY,
+          onCompleteParams: [target, direction * -1],
+        });
+      }
+      float.forEach((el) => {
+        moveX(el, 1);
+        moveY(el, -1);
+        rotate(el, 1);
       });
-      floatTl.to(float, {
-        rotation: randomAngle,
-        x: randomX,
-        y: randomY,
-        duration: randomTime,
-        ease: Sine.easeInOut,
-      }, 0);
     },
   },
 };
