e223beeccf6f1debd6579421f9552be0e2d48f7f
Tim Nokes <tim.nokes@e3creative.co.uk>
2020-11-04 10:47:19 +0000

    BEVIDAB-20 updated carousel dots

--- web/app/themes/bevi/resources/components/modules/CarouselDots/CarouselDots.vue
+++ web/app/themes/bevi/resources/components/modules/CarouselDots/CarouselDots.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="carousel-dots">
-    <div class="mr-6">
-      <img
-        :src="require('~/assets/images/icons/arrow.svg')"
-        class="transform -rotate-90 arrow"
-        @click="carouselPrev()"
+    <div
+      class="mr-6"
+      @click="carouselPrev()"
+    >
+      <svg
+        viewBox="0 0 22.136 8.483"
+        class="transform arrow"
       >
+        <path
+          id="Path_148162"
+          data-name="Path 148162"
+          d="M22.136,4.236,17.886,0,16.474,1.417,18.31,3.243H0v2H18.3L16.479,7.071,17.9,8.483Z"
+          transform="translate(22.136 8.483) rotate(180)"
+        />
+      </svg>
     </div>
     <button
       v-for="(item, index) in itemCount"
@@ -14,12 +23,21 @@
       class="carousel-dot"
       :class="{ 'is-active': index === activeSlide }"
     />
-    <div class="ml-6">
-      <img
-        :src="require('~/assets/images/icons/arrow.svg')"
-        class="transform rotate-90 arrow"
-        @click="carouselNext()"
+    <div
+      class="ml-6"
+      @click="carouselNext()"
+    >
+      <svg
+        viewBox="0 0 22.136 8.483"
+        class="transform rotate-180 arrow"
       >
+        <path
+          id="Path_148162"
+          data-name="Path 148162"
+          d="M22.136,4.236,17.886,0,16.474,1.417,18.31,3.243H0v2H18.3L16.479,7.071,17.9,8.483Z"
+          transform="translate(22.136 8.483) rotate(180)"
+        />
+      </svg>
     </div>
   </div>
 </template>
@@ -53,7 +71,8 @@
 <style lang="scss" scoped>
 .arrow {
   @apply opacity-50 cursor-pointer transition-opacity duration-200;
-  width: 15px;
+  width: 22px;
+
   &:hover {
     @apply opacity-70;
   }
