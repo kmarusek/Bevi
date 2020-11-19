cd6c5359e45368eae918c6711fabf1044d12a229
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2020-11-19 16:45:12 +0000

    BEVIDAB-27 - added acf field and component

---
+++ web/app/themes/bevi/resources/components/modules/VideoComponent/VideoComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="py-56">
+    <h2>
+      Video component
+    </h2>
+  </section>
+</template>
+
+<script>
+  export default {
+
+  };
+</script>
+
+<style>
+
+</style>
