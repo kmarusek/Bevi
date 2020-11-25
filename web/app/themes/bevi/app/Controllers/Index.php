c255912c66083938a9db24d60097074dc509dc3d
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2020-11-25 18:13:06 +0000

    BEVIDAB-33 - added content for blog page

--- web/app/themes/bevi/app/Controllers/Index.php
+++ web/app/themes/bevi/app/Controllers/Index.php
@@ -6,13 +6,4 @@ use Sober\Controller\Controller;
 
 class Index extends Controller
 {
-
-    public function hero()
-    {
-      return [
-        'title' => 'Latest News',
-        'show_bubbles' => true,
-        'wave' => '3',
-      ];
-    }
 }
