90c67bbdc4def72510800e3a4ea778e278966c6c
Tim Nokes <tim.nokes@e3creative.co.uk>
2020-12-04 10:14:32 +0000

    BEVIDAB-12 added safelist to tailwind config

--- web/app/themes/bevi/resources/assets/styles/tailwind.config.js
+++ web/app/themes/bevi/resources/assets/styles/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
+  purge: {
+    content: [ '**/*.vue' ],
+    options: {
+      safelist: [ /red$/, /yellow$/, /orange$/, /green$/, /blue$/, /purple$/ ],
+    }
+  },
   theme: {
     fontFamily: {
       body: ['Italian Plate', 'sans-serif'],
