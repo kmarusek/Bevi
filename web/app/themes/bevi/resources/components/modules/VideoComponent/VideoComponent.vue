7b433fea4e025a53a05ab44a19cbf8b1a2b15c7c
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2021-01-06 17:42:11 +0000

    BEVIDAB-116 - updated video scroll

--- web/app/themes/bevi/resources/components/modules/VideoComponent/VideoComponent.vue
+++ web/app/themes/bevi/resources/components/modules/VideoComponent/VideoComponent.vue
@@ -4,35 +4,36 @@
       <video
         v-if="block.video.url"
         id="videos"
-        :poster="block.poster.sizes.large"
         muted
         loop
         preload="auto"
         class="z-1 h-full w-full object-cover"
       >
         <source
-          :src="block.video.url"
+          :src="`${block.video.url}#t=0.1`"
           type="video/mp4"
         >
       </video>
       <div class="h-full bg-white flex items-center w-full text-area z-10 absolute top-0">
         <div class="container max-w-3xl">
           <h2
-            class="h1 scroll-animate"
+            class="h2-large scroll-animate max-w-2xl mx-auto px-4"
             v-if="block.title"
           >
             {{ block.title }}
           </h2>
           <div
+            v-if="block.text"
             v-html="block.text"
             class="mt-2 md:mt-6 block-content md:px-8 scroll-animate"
           />
         </div>
       </div>
-      <div class="h-full text-white flex items-center w-full second-text-area z-10 absolute top-0">
+      <div class="h-full flex items-center w-full second-text-area z-10 absolute top-0">
         <div class="container max-w-4xl">
           <h2
-            class="h1 text-white second-title"
+            class="h1 second-title"
+            :class="block.text_color.value"
             v-if="block.second_title"
           >
             {{ block.second_title }}
@@ -60,6 +61,14 @@
       block: {
         required: true,
         type: Object,
+      },
+    },
+    computed: {
+      isMobile() {
+        if (window.innerWidth <= 768) {
+          return true;
+        }
+        return false;
       },
     },
     mounted() {
@@ -92,14 +101,15 @@
           scrollTrigger: {
             trigger: '.parallax-container',
             pin: '.parallax-container',
-            scrub: true,
+            toggleActions: 'play complete reverse reset',
+            scrub: false,
           },
         });
         tl.to('.text-area', {
           ease: 'power1.out',
           opacity: 0,
-          duration: 1,
-        }, '+=0.3')
+          duration: 0.2,
+        }, '+=0.6')
           .call(this.playVideo)
           .to('.second-title', {
             ease: 'power1.out',
@@ -127,5 +137,46 @@ video {
   bottom: 10%;
   left: 50%;
   transform: translateX(-50%);
+}
+
+.h2-large {
+  @apply font-body text-3xl text-blue-600 font-semibold antialiased;
+
+  @screen md {
+    @apply text-4xl leading-tight;
+  }
+  @screen lg {
+    font-size: 48px;
+  }
+}
+
+.second-title {
+  .text-blue-600 {
+    @apply text-blue-600;
+  }
+  .text-white {
+    @apply text-white;
+  }
+  .text-blue {
+    @apply text-blue;
+  }
+  .text-green {
+    @apply text-green;
+  }
+  .text-purple {
+    @apply text-purple;
+  }
+  .text-red {
+    @apply text-red;
+  }
+  .text-orange {
+    @apply text-orange;
+  }
+  .text-yellow {
+    @apply text-yellow;
+  }
+  .text-gray {
+    @apply text-gray;
+  }
 }
 </style>
