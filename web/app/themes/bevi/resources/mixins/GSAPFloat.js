b3b6a14115caa3d8c3a9d09ad60d6ce01caad93d
Tim Nokes <tim.nokes@e3creative.co.uk>
2021-01-20 16:22:03 +0000

    BEVIDAB-130 updated float function

--- web/app/themes/bevi/resources/mixins/GSAPFloat.js
+++ web/app/themes/bevi/resources/mixins/GSAPFloat.js
@@ -11,6 +11,7 @@ export default {
         const target = section.querySelectorAll('.gsap-float');
 
         const float = gsap.timeline({ repeat: -1, yoyo: true, repeatRefresh: true });
+        const randomDuration = gsap.utils.random(5, 7, 10);
 
         float.from(target, {
           rotate: 0,
@@ -19,10 +20,11 @@ export default {
         });
 
         float.to(target, {
-          rotation: 'random(-5, 5)',
-          x: 'random(-15, 15)',
-          y: 'random(-15, 15)',
-          duration: 'random(5, 10)',
+          rotation: 'random(-8, 8)',
+          x: 'random(-20, 20)',
+          y: 'random(-20, 20)',
+          duration: randomDuration,
+          stagger: 0.1,
           ease: Sine.easeInOut,
           transformOrigin: '50% 50%',
         });
@@ -39,48 +41,5 @@ export default {
         });
       });
     },
-    // float(elems) {
-    //   function random(min, max) {
-    //     const delta = max - min;
-    //     return (direction = 1) => (min + delta * Math.random()) * direction;
-    //   }
-
-    //   const randomX = random(2, 12);
-    //   const randomY = random(2, 12);
-    //   const randomTime = random(3, 5);
-    //   const randomTime2 = random(5, 10);
-    //   const randomAngle = random(-4, 4);
-    //   const float = elems;
-
-    //   function rotate(target, direction) {
-    //     gsap.to(target, randomTime2(), {
-    //       rotation: randomAngle(direction),
-    //       ease: Sine.easeInOut,
-    //       onComplete: rotate,
-    //       onCompleteParams: [target, direction * -1],
-    //     });
-    //   }
-    //   function moveX(target, direction) {
-    //     gsap.to(target, randomTime(), {
-    //       x: randomX(direction),
-    //       ease: Sine.easeInOut,
-    //       onComplete: moveX,
-    //       onCompleteParams: [target, direction * -1],
-    //     });
-    //   }
-    //   function moveY(target, direction) {
-    //     gsap.to(target, randomTime(), {
-    //       y: randomY(direction),
-    //       ease: Sine.easeInOut,
-    //       onComplete: moveY,
-    //       onCompleteParams: [target, direction * -1],
-    //     });
-    //   }
-    //   float.forEach((el) => {
-    //     moveX(el, 1);
-    //     moveY(el, -1);
-    //     rotate(el, 1);
-    //   });
-    // },
   },
 };
