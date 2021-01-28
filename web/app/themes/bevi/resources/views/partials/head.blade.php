818c35136dc0860ec080542464881aa44e0286b9
Nick Martinez - Meakins <nick.martinez@e3creative.co.uk>
2021-01-28 13:51:35 +0000

    BEVIDAB-141 - removing cookiebot

--- web/app/themes/bevi/resources/views/partials/head.blade.php
+++ web/app/themes/bevi/resources/views/partials/head.blade.php
@@ -3,7 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
-  <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="87b4ec8d-7070-4d14-a5f7-e12be46378df" data-blockingmode="auto" type="text/javascript"></script>
   <script src="https://www.workable.com/assets/embed.js" type="text/javascript"></script>
   <script type="text/javascript" charset="utf-8">
   whr(document).ready(function(){
