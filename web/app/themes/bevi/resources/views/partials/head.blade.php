d19703ae30382ca58fbb77d0442c43bc9e0629a2
Tom Hartley <tom.hartley@deptagency.com>
2021-02-22 16:45:55 +0000

    BEVSUP-11: Move script

--- web/app/themes/bevi/resources/views/partials/head.blade.php
+++ web/app/themes/bevi/resources/views/partials/head.blade.php
@@ -5,14 +5,14 @@
   @php wp_head() @endphp
   @if (is_page('careers'))
     <script src="https://www.workable.com/assets/embed.js" type="text/javascript"></script>
+    <script type="text/javascript" charset="utf-8">
+      whr(document).ready(function(){
+        // whr_embed(250461, {detail: 'titles', base: 'jobs', zoom: 'country', grouping: 'none'});
+        whr_embed(250461, {detail: 'titles', base: 'departments', zoom: 'city', });
+      });
+    </script>
   @endif
   <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="87b4ec8d-7070-4d14-a5f7-e12be46378df" data-blockingmode="auto" type="text/javascript"></script>
-  <script type="text/javascript" charset="utf-8">
-  whr(document).ready(function(){
-  // whr_embed(250461, {detail: 'titles', base: 'jobs', zoom: 'country', grouping: 'none'});
-  whr_embed(250461, {detail: 'titles', base: 'departments', zoom: 'city', });
-  });
-  </script>
   <script type="text/javascript" src="//cdn.bizible.com/scripts/bizible.js" async=""></script>
 
   <!-- Google Tag Manager -->
