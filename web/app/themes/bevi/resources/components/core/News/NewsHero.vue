35a2a6b4f9bbf36593d83b14ba63d4e29aefe191
Tom Hartley <tom.hartley@deptagency.com>
2021-03-01 14:00:59 +0000

    BEVSUP-19: Update single blog page padding

--- web/app/themes/bevi/resources/components/core/News/NewsHero.vue
+++ web/app/themes/bevi/resources/components/core/News/NewsHero.vue
@@ -1,31 +1,33 @@
 <template>
   <section class="relative">
     <div class="flex flex-col container relative pt-24 md:pt-40">
-      <div class="flex items-center flex-wrap">
-        <CategoryButton
-          :category="cardCategory[0]"
-          class="mr-4 md:mr-6"
-        />
-      </div>
-      <h2 class="h2 mt-6 md:mt-10 mb-8 md:w-2/3">
-        {{ postData.post_title }}
-      </h2>
-      <div class="flex mb-8 md:mb-24">
-        <img
-          v-if="postData.post_author_avatar"
-          :src="postData.post_author_avatar"
-          alt="avatar"
-          class="avatar "
-        >
-        <div class="flex flex-col justify-center">
-          <div class="capitalize h5">
-            {{ postData.post_author }}
-          </div>
-          <div
-            v-if="postData.post_author_role"
-            class="text-xs text-gray-700"
+      <div class="w-full lg:w-10/12 mx-auto md:px-8">
+        <div class="flex items-center flex-wrap">
+          <CategoryButton
+            :category="cardCategory[0]"
+            class="mr-4 md:mr-6"
+          />
+        </div>
+        <h2 class="h2 mt-6 md:mt-10 mb-8 md:w-2/3">
+          {{ postData.post_title }}
+        </h2>
+        <div class="flex mb-8 md:mb-24">
+          <img
+            v-if="postData.post_author_avatar"
+            :src="postData.post_author_avatar"
+            alt="avatar"
+            class="avatar "
           >
-            {{ postData.post_author_role }}
+          <div class="flex flex-col justify-center">
+            <div class="capitalize h5">
+              {{ postData.post_author }}
+            </div>
+            <div
+              v-if="postData.post_author_role"
+              class="text-xs text-gray-700"
+            >
+              {{ postData.post_author_role }}
+            </div>
           </div>
         </div>
       </div>
