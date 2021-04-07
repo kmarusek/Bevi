f1a2ff72039ae4b02af21f0985f733dd28277365
Tom Hartley <tom.hartley@deptagency.com>
2021-04-07 12:58:22 +0100

    BEVSUP-21: Add form components

---
+++ web/app/themes/bevi/resources/components/common/Input/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <input
+    :type="type"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+    class="appearance-none text-sm w-full py-4 px-2 outline-none border-b-2 focus:border-blue focus:bg-white bg-gray-100 duration-300 border-blue-200 transition-all font-light"
+  />
+</template>
+
+<script>
+  export default {
+    props: {
+      type: {
+        type: String,
+        default: 'text',
+      },
+      value: {
+        type: String,
+        default: '',
+      },
+    },
+  };
+</script>
---
+++ web/app/themes/bevi/resources/components/common/Label/Label.vue
@@ -0,0 +1,11 @@
+<template>
+  <label class="text-xs block mb-1 text-black font-medium">
+    <slot />
+  </label>
+</template>
+
+<script>
+  export default {
+
+  };
+</script>
