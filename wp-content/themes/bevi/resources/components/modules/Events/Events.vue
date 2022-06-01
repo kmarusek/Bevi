b8a0230dd1c7f5c6b8e8e91575a079bb5cb85781
lbognar-m <bognar.lorand@gmail.com>
2022-06-01 17:43:58 +0300

    fix fields

--- wp-content/themes/bevi/resources/components/modules/Events/Events.vue
+++ wp-content/themes/bevi/resources/components/modules/Events/Events.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container py-8 lg:py-32 lg:px-12 mx-auto">
-    <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    <div class="relative flex min-h-screen flex-col overflow-hidden bg-gray-50 py-6 sm:py-12">
       <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:rounded-lg sm:px-10">
 
         <div
@@ -10,13 +10,13 @@
             :id="`item-${idx}`">
 
           <div class="col-span-1 py-8 text-base leading-7 text-gray-600">
-            <img class="object-contain" src="{{ item.event_image }}" />
+            <img v-bind:src="item.event_image" class="w-48" />
           </div>
           <div class="col-span-4 space-y-3 py-8 text-base leading-7 text-gray-600">
             <p class="font-bold">{{ item.event_date }}</p>
             <p class="font-bold">{{ item.event_title }}</p>
             <p>{{ item.event_description }}</p>
-            <p><a href="{{ item.event_location }}">{{ item.event_location }}</a></p>
+            <p><a v-bind:src="item.event_location">{{ item.event_location }}</a></p>
           </div>
 
         </div>
