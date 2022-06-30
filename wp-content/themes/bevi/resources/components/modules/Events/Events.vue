ada940f0b55c2fe9570f8c8f99927c2984e8cc35
lbognar-m <bognar.lorand@gmail.com>
2022-06-30 16:12:03 +0300

    mobile layout

--- wp-content/themes/bevi/resources/components/modules/Events/Events.vue
+++ wp-content/themes/bevi/resources/components/modules/Events/Events.vue
@@ -2,14 +2,14 @@
   <section class="container mx-auto bg-gray-100">
     <div class="relative flex min-h-screen flex-col overflow-hidden">
 
-      <div class="block-content py-16 px-36" data-v-c5a16a1c="">
+      <div class="block-content py-16 px-8 md:px-36" data-v-c5a16a1c="">
         <h1 class="gsap-fades my-4 heading-two">{{ block.events_title }}</h1>
         <p>{{ block.events_description }}</p>
         <hr class="mt-16" />
       </div>
 
-      <div class="eventbuttons px-36">
-        <button class="future btn mr-16">Future events</button>
+      <div class="eventbuttons px-8 md:px-36">
+        <button class="future btn mr-4 md:mr-16">Future events</button>
         <button class="past btn bg-gray-300">Past events</button>
       </div>
 
@@ -17,12 +17,12 @@
           v-for="(item, idx) in block.r_event"
           :key="idx"
           :id="`item-${idx}`"
-          class="eventcard mx-36 my-16 ring-1 ring-gray-900/5 sm:mx-auto sm:rounded-lg sm:px-10 bg-white border border-white hover:border-blue-200">
+          class="eventcard mx-8 md:mx-36 my-16 ring-1 ring-gray-900/5 sm:mx-auto sm:rounded-lg sm:px-10 bg-white border border-white hover:border-blue-200">
 
         <div class="grid grid-cols-5 gap-4">
-          <div v-bind:style="{ 'background-image': 'url(' + item.event_image + ')' }" class="col-span-1 my-8 mr-8 text-base leading-7 text-gray-600 rounded-full w-48 h-48 bg-cover">
+          <div v-bind:style="{ 'background-image': 'url(' + item.event_image + ')' }" class="col-span-5 md:col-span-1 my-8 mx-auto md:mx-8 text-base leading-7 text-gray-600 rounded-full w-48 h-48 bg-cover">
           </div>
-          <div class="col-span-4 space-y-3 py-8 text-base leading-7 text-gray-600">
+          <div class="col-span-4 space-y-3 py-8 px-8 md:px-0 text-base leading-7 text-gray-600">
             <p class="eventdate font-bold">{{ item.event_date }}</p>
             <h3 class="text-3xl md:text-4xl mb-4 text-blue-600 font-semibold leading-tight eventcard-hover:text-blue">{{ item.event_title }}</h3>
             <p>{{ item.event_time }}</p>
