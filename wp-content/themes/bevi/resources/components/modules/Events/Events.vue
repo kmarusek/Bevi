e0b32f30a2fea72db765560267ed0847e5c67159
lbognar-m <bognar.lorand@gmail.com>
2022-07-18 23:22:38 +0300

    mobile fixes

--- wp-content/themes/bevi/resources/components/modules/Events/Events.vue
+++ wp-content/themes/bevi/resources/components/modules/Events/Events.vue
@@ -2,31 +2,45 @@
   <section class="container mx-auto bg-gray-100">
     <div class="relative flex min-h-screen flex-col overflow-hidden">
 
-      <div class="block-content py-16 pb-4 px-8 md:px-36" data-v-c5a16a1c="">
+      <div class="block-content py-16 pb-4 px-0 lg:px-48" data-v-c5a16a1c="">
         <h1 class="gsap-fades my-4 heading-two">{{ block.events_title }}</h1>
-        <p>{{ block.events_description }}</p>
+        <div class="grid grid-cols-5 gap-4">
+          <div class="col-start-1 col-end-6 md:col-end-3">
+            <p>{{ block.events_description }}</p>
+          </div>
+          <div class="col-start-1 col-end-6 md:col-start-4">
+            <a
+                v-if="block.events_contact_button"
+                v-bind:href="block.events_contact_button.url"
+                class="px-3 md:px-10 text-blue bg-white border border-2 border-blue hover:text-white hover:bg-blue w-full rounded-full py-3 inline-block text-center">
+                Go to event
+            </a>
+          </div>
+        </div>
         <hr class="mt-6" />
       </div>
 
-      <div class="eventbuttons py-4 px-8 md:px-36">
-        <button class="future btn mr-4 md:mr-16">Future events</button>
-        <button class="past btn bg-gray-300">Past events</button>
+      <div class="eventbuttons py-4 px-0 lg:px-48 flex flex-columns items-center">
+        <button class="future btn px-3 md:px-10 mr-4 md:mr-16 w-1/2 md:w-64">Future events</button>
+        <button class="past btn px-3 md:px-10 bg-gray-300 w-1/2 md:w-64">Past events</button>
       </div>
 
       <div
           v-for="(item, idx) in block.r_event"
           :key="idx"
           :id="`item-${idx}`"
-          class="eventcard mx-8 md:mx-36 my-16 ring-1 ring-gray-900/5 sm:mx-auto sm:rounded-lg sm:px-10 bg-white border border-white hover:border-blue-200">
+          class="eventcard mx-0 lg:mx-48 my-8 ring-1 ring-gray-900/5 rounded-3xl sm:px-10 bg-white border border-white hover:border-blue-200">
 
         <div class="grid grid-cols-5 gap-4">
-          <div v-bind:style="{ 'background-image': 'url(' + item.event_image + ')' }" class="col-span-5 md:col-span-1 my-8 mx-auto md:mx-8 text-base leading-7 text-gray-600 rounded-full w-48 h-48 bg-cover">
+          <div v-bind:style="{ 'background-image': 'url(' + item.event_image + ')' }" class="col-span-5 md:col-span-2 xl:col-span-1 my-8 mx-auto text-base leading-7 text-gray-600 rounded-full w-48 h-48 bg-cover relative clip-polygon">
+            <!-- ImageBlob :image="item.event_image" /-->
+
           </div>
-          <div class="col-span-4 space-y-3 py-8 px-8 md:px-0 text-base leading-7 text-gray-600">
-            <p class="eventdate font-bold">{{ item.event_date }}</p>
+          <div class="col-start-1 md:col-start-3 xl:col-start-2 col-end-6 space-y-3 py-8 px-8 md:px-0 text-base leading-7 text-gray-600">
+            <p class="eventdate text-2xl">{{ item.event_date }}</p>
             <h3 class="text-3xl md:text-4xl mb-4 text-blue-600 font-semibold leading-tight eventcard-hover:text-blue">{{ item.event_title }}</h3>
-            <p>{{ item.event_time }}</p>
-            <p><a v-bind:href="item.event_location">{{ item.event_location }}</a></p>
+            <p class="text-xl">{{ item.event_time }}</p>
+            <p><a v-if="item.event_location" v-bind:href="item.event_location.url">{{ item.event_location.title }}</a></p>
             <p class="text-body">{{ item.event_description }}</p>
           </div>
         </div>
@@ -54,14 +68,14 @@
         }
         jQuery(this).closest('.eventcard').addClass(dateclass);
       });
-      jQuery( ".eventcard.past" ).hide();
-      jQuery( ".eventbuttons .btn.future" ).click(function() {
+      jQuery(   ".eventcard.past" ).hide();
+      jQuery(   ".eventbuttons .btn.future" ).click(function() {
         jQuery( ".eventbuttons .btn.past" ).addClass('bg-gray-300');
         jQuery( ".eventbuttons .btn.future" ).removeClass('bg-gray-300');
         jQuery( ".eventcard.future" ).show();
         jQuery( ".eventcard.past" ).hide();
       });
-      jQuery( ".eventbuttons .btn.past" ).click(function() {
+      jQuery(   ".eventbuttons .btn.past" ).click(function() {
         jQuery( ".eventbuttons .btn.future" ).addClass('bg-gray-300');
         jQuery( ".eventbuttons .btn.past" ).removeClass('bg-gray-300');
         jQuery( ".eventcard.past" ).show();
@@ -74,5 +88,12 @@
 <style>
 .eventcard:hover .eventcard-hover\:text-blue {
   color: #246eff;
+}
+.clippath-blob {
+  -webkit-clip-path: url(#shape);
+  clip-path: url(#shape);
+}
+.clip-polygon {
+  clip-path: circle(50% at center);
 }
 </style>
