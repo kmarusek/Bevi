6a4a1d1b3f1500bc9956a3827084232cd37285e5
lbognar-m <bognar.lorand@gmail.com>
2022-08-26 15:09:24 +0300

    ipad pro fix

--- wp-content/themes/bevi/resources/components/modules/HeroBanner/HeroBanner.vue
+++ wp-content/themes/bevi/resources/components/modules/HeroBanner/HeroBanner.vue
@@ -95,7 +95,7 @@
       </div>
       <div
         v-if="block.feature_image"
-        class="flex-1 block flex mt-10 pt-32 lg:mt-0 -z-10"
+        class="flex-1 block flex mt-10 pt-2 xl:pt-32 lg:mt-0 -z-10"
         :class="block.align_feature_image_bottom ? 'items-end' : 'items-center'"
       >
         <img
