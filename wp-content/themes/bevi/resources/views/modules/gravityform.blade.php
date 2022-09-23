91df2dcc9e0a7fabac28db052599c5764f4d5704
lbognar-m <bognar.lorand@gmail.com>
2022-09-23 19:28:16 +0300

    Added ajax

--- wp-content/themes/bevi/resources/views/modules/gravityform.blade.php
+++ wp-content/themes/bevi/resources/views/modules/gravityform.blade.php
@@ -1,4 +1,4 @@
 @php
-    $block['the_form'] = do_shortcode('[gravityform id="' . $block['select_form'] . '" title="false"]');
+    $block['the_form'] = do_shortcode('[gravityform id="' . $block['select_form'] . '" title="false" ajax="true"]');
 @endphp
 <gravity-form :block="{{ json_encode($block) }}"></gravity-form>
