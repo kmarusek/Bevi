1443084fb6d678591decd1a8de59ad8029543a25
lbognar-m <bognar.lorand@gmail.com>
2022-09-23 19:36:16 +0300

    Temporarily disabled ajax

--- wp-content/themes/bevi/resources/views/modules/gravityform.blade.php
+++ wp-content/themes/bevi/resources/views/modules/gravityform.blade.php
@@ -1,4 +1,4 @@
 @php
-    $block['the_form'] = do_shortcode('[gravityform id="' . $block['select_form'] . '" title="false" ajax="true"]');
+    $block['the_form'] = do_shortcode('[gravityform id="' . $block['select_form'] . '" title="false" ajax="false"]');
 @endphp
 <gravity-form :block="{{ json_encode($block) }}"></gravity-form>
