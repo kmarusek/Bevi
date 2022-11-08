07344f673a5a2f57aa80414ef20302dfdb9e753d
kmarusek <kevinm@abovemediateam.com>
2022-11-08 14:24:18 -0500

    style

--- wp-content/themes/bevi/resources/components/modules/GravityForm/GravityForm.vue
+++ wp-content/themes/bevi/resources/components/modules/GravityForm/GravityForm.vue
@@ -158,22 +158,8 @@
   font-weight: 600;
   position: relative !important;
 }
-.quiz-result-cta:before{
-  position: absolute;
-  width: 100%;
-  height: 1.5px;
-  background: #246eff;
-  content: ' ';
-  top: 100%;
-  left: 0;
-  pointer-events: none;
-  transform-origin: 100% 50%;
-  transform: scale3d(0,1,1);
-  transition: transform 0.3s;
-}
-.quiz-result-cta:hover:before{
-  transform-origin: 0% 50%;
-  transform: scale3d(1,1,1);
+.quiz-result-cta:hover {
+  color: #5179CC;
 }
 .quiz-terms {
   font-size: 10px;
