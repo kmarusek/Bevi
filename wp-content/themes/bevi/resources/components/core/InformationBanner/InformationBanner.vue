6a43e2eb9d88a51289f5830b59f71020cc6d563d
kmarusek <kevinm@abovemediateam.com>
2022-11-28 10:28:56 -0500

    banner changes

--- wp-content/themes/bevi/resources/components/core/InformationBanner/InformationBanner.vue
+++ wp-content/themes/bevi/resources/components/core/InformationBanner/InformationBanner.vue
@@ -32,9 +32,10 @@
 </script>
 
 <style lang="scss" scoped>
+
     .informationBanner-wrap {
         background-color: #246eff;
-        padding: 8px 0px;
+        padding: 8px 15px;
         color: #ffffff;
         display:flex;
         justify-content: center;
@@ -42,26 +43,40 @@
         margin-bottom: 16px;
         align-items: center;
         font-family: Space Grotesk,sans-serif;
+        p{
+            font-size: 14px;
+        }
+        a {
+            @apply relative font-space font-semibold transition-all duration-300 inline items-center;
+                font-size: 14px;
+            &:after {
+              @apply absolute w-full left-0 bg-white transition-all duration-300;
+              content: "";
+              height: 2px;
+              margin-top: 2px;
+              top: 100%;
+              transform: scaleX(0) translateY(-2px);
+              transform-origin: left;
+              z-index: 0;
+            }
+            &:hover,
+            &.active {
+              &:after {
+                transform: scaleX(1) translateY(-2px);
+              }
+            }
+        } 
     }
-    .informationBanner-wrap  a {
-    @apply relative font-space font-semibold transition-all duration-300 inline items-center;
-        font-size: 14px;
-    &:after {
-      @apply absolute w-full left-0 bg-white transition-all duration-300;
-      content: "";
-      height: 2px;
-      margin-top: 2px;
-      top: 100%;
-      transform: scaleX(0) translateY(-2px);
-      transform-origin: left;
-      z-index: 0;
+
+  @media screen and (max-width: 450px) {
+    .informationBanner-wrap {
+        p{
+            font-size: 12px;
+        }
+        a{
+            font-size: 12px;
+        }
     }
-    &:hover,
-    &.active {
-      &:after {
-        transform: scaleX(1) translateY(-2px);
-      }
-    }
-  }
+}
     
 </style>
