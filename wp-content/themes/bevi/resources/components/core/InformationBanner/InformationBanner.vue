89844c773cc1ae54c816491c33575e62b77e7d62
kmarusek <kevinm@abovemediateam.com>
2022-11-28 17:25:43 -0500

    add btn styles, responsive code and dynamic color picking

--- wp-content/themes/bevi/resources/components/core/InformationBanner/InformationBanner.vue
+++ wp-content/themes/bevi/resources/components/core/InformationBanner/InformationBanner.vue
@@ -1,18 +1,26 @@
 <template>
 
-    <section class="w-full informationBanner-wrap"
+    <section class="w-full informationBanner-wrap "
+    :style="colorStyles" 
     v-if="banner.show === true"
     >
+     <div class="informationBanner-content">
         <p>
         {{banner.text}}
+        </p>
+     </div> 
+     <div class="informationBanner-cta">
         <a v-if="banner.cta.url"
         :href="banner.cta.url"
         :target="banner.cta.taget ?? '_self'"
         >
-        {{ banner.cta.title }}
+          <div class="informationBanner-btn">
+            <p>
+            {{ banner.cta.title }}
+            </p>
+          </div>
         </a>
-        </p>
-
+     </div>
     </section>
 
 </template>
@@ -25,6 +33,13 @@
                 type: Object,
             },
         },
+        computed: {
+            colorStyles(){
+                return {
+                    "background-color": this.banner.color
+                };
+            }
+        },
         mounted () {
             //all scripts
         },
@@ -34,47 +49,70 @@
 <style lang="scss" scoped>
 
     .informationBanner-wrap {
-        background-color: #246eff;
-        padding: 8px 15px;
-        color: #ffffff;
+        padding: 8px;
         display:flex;
-        justify-content: center;
+        justify-content: space-around;
+        align-items: center;
+        flex-wrap: nowrap;
         margin-top: -16px;
         margin-bottom: 16px;
-        align-items: center;
+        height: 64px;
         font-family: Space Grotesk,sans-serif;
-        p{
-            font-size: 14px;
+       .informationBanner-content {
+            width: 50%;
+            p{
+            font-size: 16px;
+            line-height: 30px;
+            color: #fafafa;
+            letter-spacing: 0.64px;
+            margin-right: 18px;
+            text-align: right;
+            }
+       }
+        .informationBanner-cta{ 
+            width: 50%;
+            a .informationBanner-btn {
+            border: 1px solid #fafafa;
+            background-color: transparent;
+            border-radius: 24px;
+            padding: 11px 14px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            max-width: 200px;
+            transition: .3s all;
+                p{
+                font-size: 16px;
+                font-weight: 700;
+                line-height: 14px;
+                color: #fafafa;
+                transition: .3s all;
+                }
+            } 
+            a:hover .informationBanner-btn{
+                background-color: #fafafa;
+                transition: .3s all;
+                p{
+                    color: #00223e;
+                    transition: .3s all;
+                }
+            }
         }
-        a {
-            @apply relative font-space font-semibold transition-all duration-300 inline items-center;
-                font-size: 14px;
-            &:after {
-              @apply absolute w-full left-0 bg-white transition-all duration-300;
-              content: "";
-              height: 2px;
-              margin-top: 2px;
-              top: 100%;
-              transform: scaleX(0) translateY(-2px);
-              transform-origin: left;
-              z-index: 0;
-            }
-            &:hover,
-            &.active {
-              &:after {
-                transform: scaleX(1) translateY(-2px);
-              }
-            }
-        } 
     }
 
-  @media screen and (max-width: 450px) {
+  @media screen and (max-width: 800px) {
     .informationBanner-wrap {
-        p{
-            font-size: 12px;
+        .informationBanner-content{
+            width: 60%;
+            p{
+            font-size: 14px;
+            line-height: 24px;
+            letter-spacing: .56px;
+            text-align: left;
+            }
         }
-        a{
-            font-size: 12px;
+        .informationBanner-cta{ 
+            width: 40%;
         }
     }
 }
