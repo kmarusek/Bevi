dbfdfe09ceef557655667b678c988b0b15fbd23f
kmarusek <kevinm@abovemediateam.com>
2022-12-08 16:36:28 -0500

    testing form settings and responsive for pardot import

--- wp-content/themes/bevi/resources/components/modules/PageHeroWithFormAndVideo/PageHeroWithFormAndVideo.vue
+++ wp-content/themes/bevi/resources/components/modules/PageHeroWithFormAndVideo/PageHeroWithFormAndVideo.vue
@@ -29,7 +29,7 @@
 
     </div>
 
-    <div class="container flex flex-col lg:flex-row mb-9.5">
+    <div class="container flex flex-col lg:flex-row">
       <div class="grid grid-cols-2 xl:grid-cols-8 items-end justify-between pt-20 xs:py-32 w-full lg:gap-20">
         <div class="sm:col-span-1 xl:col-span-3 space-y-4 pb-20">
           <h3
