705c662886c1bd9e453ba1b14d02c3d0b75b012d
kmarusek <kevinm@abovemediateam.com>
2023-01-11 14:52:22 -0500

    add nitropack

---
+++ wp-content/plugins/nitropack/nitropack-sdk/vendor/composer/installed.php
@@ -0,0 +1,45 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nitropack/nitropackcloud-sdk',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '20f7129199b630859b03f00472d146372ba13638',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nitropack/httpclient' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8744838023ab86b9ce8d754ae04cc4095a54d9b8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nitropack/httpclient',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'nitropack/nitropackcloud-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '20f7129199b630859b03f00472d146372ba13638',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nitropack/url' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a8ea4e71f231de2e3527f716d3a08946048524fc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nitropack/url',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
